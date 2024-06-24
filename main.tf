81399e640f1f6a8bf31e0f92710e2e7660c5d7bc
gary.ma <garymar2007@gmail.com>
2024-06-24 19:42:41 +0200

    ec2 deployment example

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
